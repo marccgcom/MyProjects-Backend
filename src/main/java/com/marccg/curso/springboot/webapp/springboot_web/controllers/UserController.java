7d804f80738f2b06a5c14ce1132b576d65b91661
Marc <marc.cabre.guinovart@gmail.com>
2025-03-29 15:15:15 +0100

    datos a la vista con map

--- src/main/java/com/marccg/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/marccg/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.marccg.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserController {
     
     @GetMapping("/details")
-    public String details(Model model) {
-        model.addAttribute("title", "Hola  mundo spring boot");
-        model.addAttribute("name", "Marc");
-        model.addAttribute("lastname", "CG");
+    public String details(Map<String, Object> model) {
+        model.put("title", "Hola  mundo spring boot");
+        model.put("name", "Marc");
+        model.put("lastname", "CG");
         return "details";
     }
 }

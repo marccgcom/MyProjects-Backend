3772933c93669daf2e46409baec4465017e698f2
Marc <marc.cabre.guinovart@gmail.com>
2025-03-29 15:28:24 +0100

    requestmapping

--- src/main/java/com/marccg/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/marccg/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -3,13 +3,15 @@ package com.marccg.curso.springboot.webapp.springboot_web.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
     
-    @GetMapping("/details-rest")
+    @RequestMapping(path="/details", method=RequestMethod.GET)
     public Map<String, Object> details() {
         Map<String, Object> response = new HashMap<>();
 

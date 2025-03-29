5ff64a6011ec74c5ebc97c870cd3caf4d2acf608
Marc <marc.cabre.guinovart@gmail.com>
2025-03-29 18:05:54 +0100

    request

--- src/main/java/com/marccg/curso/springboot/webapp/springboot_web/controllers/RequestParamController.java
+++ src/main/java/com/marccg/curso/springboot/webapp/springboot_web/controllers/RequestParamController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.marccg.curso.springboot.webapp.springboot_web.Models.dto.ParamDto;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,6 +29,20 @@ public class RequestParamController {
     public ParamDto bar(@RequestParam() String text, @RequestParam Integer code) {
         ParamDto params = new ParamDto();
         params.setMessage(text);
+        params.setCode(code);
+        return params;
+    }
+    
+    @GetMapping("/request")
+    public ParamDto request(HttpServletRequest request) {
+        ParamDto params = new ParamDto();
+        Integer code = 0;
+        try{
+            code = Integer.parseInt(request.getParameter("code"));
+        } catch (NumberFormatException e){
+            code = null;
+        }
+        params.setMessage(request.getParameter("text"));
         params.setCode(code);
         return params;
     }

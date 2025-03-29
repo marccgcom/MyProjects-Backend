87ab8e83a4feb9e486b88b1484fa728b123bcabf
Marc <marc.cabre.guinovart@gmail.com>
2025-03-29 17:33:56 +0100

    multiple parameters

--- src/main/java/com/marccg/curso/springboot/webapp/springboot_web/Models/dto/ParamDto.java
+++ src/main/java/com/marccg/curso/springboot/webapp/springboot_web/Models/dto/ParamDto.java
@@ -3,6 +3,7 @@ package com.marccg.curso.springboot.webapp.springboot_web.Models.dto;
 public class ParamDto {
 
     private String message;
+    private Integer code;
 
     public String getMessage() {
         return message;
@@ -10,6 +11,14 @@ public class ParamDto {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
     }
 
     

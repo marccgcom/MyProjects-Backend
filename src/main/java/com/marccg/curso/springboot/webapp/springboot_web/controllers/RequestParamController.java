3c794c977527dd18b69fbc8ffb190d3da17969b3
Marc <marc.cabre.guinovart@gmail.com>
2025-03-29 16:47:29 +0100

    param not required

--- src/main/java/com/marccg/curso/springboot/webapp/springboot_web/controllers/RequestParamController.java
+++ src/main/java/com/marccg/curso/springboot/webapp/springboot_web/controllers/RequestParamController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class RequestParamController {
     
     @GetMapping("/foo")
-    public ParamDto foo(@RequestParam String message) {
+    public ParamDto foo(@RequestParam(required = false) String message) {
         ParamDto param = new ParamDto();
         param.setMessage(message);
         return param;

9a62fa383908964d0b7738b4b45c36817ae95e35
Marc <marc.cabre.guinovart@gmail.com>
2025-05-03 22:44:32 +0200

    task controller

---
+++ src/main/java/com/marccg/curso/springboot/webapp/springboot_web/controllers/TaskController.java
@@ -0,0 +1,76 @@
+package com.marccg.curso.springboot.webapp.springboot_web.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marccg.curso.springboot.webapp.springboot_web.Models.Task;
+import com.marccg.curso.springboot.webapp.springboot_web.services.TaskService;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+    
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        return taskService.findById(id)
+        .map(ResponseEntity::ok)
+        .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task status) {
+        return taskService.save(status);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
+        return taskService.findById(id).map(task -> {
+            task.setName(updatedTask.getName());
+            task.setStatus(updatedTask.getStatus());
+            task.setTaskType(updatedTask.getTaskType());
+            task.setScheduledTotalTime(updatedTask.getScheduledTotalTime());
+            task.setRealTotalTime(updatedTask.getRealTotalTime());
+            task.setTimeRecords(updatedTask.getTimeRecords());
+            task.setAssignedUsers(updatedTask.getAssignedUsers());
+            task.setProject(updatedTask.getProject());
+            return ResponseEntity.ok(taskService.save(task));
+        })
+        .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        if (taskService.findById(id).isPresent()) {
+            taskService.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+    
+
+
+}

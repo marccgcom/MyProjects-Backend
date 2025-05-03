05f145aee59268af5608ce7a4a06fcce57094792
Marc <marc.cabre.guinovart@gmail.com>
2025-05-03 23:56:19 +0200

    user reserved name solved

--- src/main/java/com/marccg/curso/springboot/webapp/springboot_web/Models/Client.java
+++ src/main/java/com/marccg/curso/springboot/webapp/springboot_web/Models/Client.java
@@ -1,10 +1,12 @@
 package com.marccg.curso.springboot.webapp.springboot_web.Models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 
 
 @Entity
+
 public class Client {
     @Id @GeneratedValue
     private Long id;

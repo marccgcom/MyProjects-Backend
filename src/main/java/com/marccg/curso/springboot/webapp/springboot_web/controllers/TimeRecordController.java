a42c74cc65b86c23496ea4256fad5a607286967c
Marc <marc.cabre.guinovart@gmail.com>
2025-05-03 23:49:37 +0200

    TimeRecords controller

---
+++ src/main/java/com/marccg/curso/springboot/webapp/springboot_web/controllers/TimeRecordController.java
@@ -0,0 +1,69 @@
+package com.marccg.curso.springboot.webapp.springboot_web.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marccg.curso.springboot.webapp.springboot_web.Models.TimeRecord;
+import com.marccg.curso.springboot.webapp.springboot_web.services.TimeRecordService;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+
+@RestController
+@RequestMapping("/api/time_records")
+public class TimeRecordController {
+    
+    private final TimeRecordService timeRecordService;
+
+    public TimeRecordController(TimeRecordService timeRecordService) {
+        this.timeRecordService = timeRecordService;
+    }
+
+    @GetMapping
+    public List<TimeRecord> getAllTimeRecords() {
+        return timeRecordService.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<TimeRecord> getTimeRecordById(@PathVariable Long id) {
+        return timeRecordService.findById(id)
+        .map(ResponseEntity::ok)
+        .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public TimeRecord createTimeRecord(@RequestBody TimeRecord timerRecord) {
+        return timeRecordService.save(timerRecord);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TimeRecord> updateTimeRecord(@PathVariable Long id, @RequestBody TimeRecord updatedTimeRecord) {
+        return timeRecordService.findById(id).map(timeRecord -> {
+            timeRecord.setDatetime(updatedTimeRecord.getDatetime());
+            timeRecord.setDedicatedHours(updatedTimeRecord.getDedicatedHours());
+            timeRecord.setTask(updatedTimeRecord.getTask());
+            return ResponseEntity.ok(timeRecordService.save(timeRecord));
+        })
+        .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTimeRecord(@PathVariable Long id) {
+        if (timeRecordService.findById(id).isPresent()) {
+            timeRecordService.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+    
+}
